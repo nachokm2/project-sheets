d51765744151deba30eb3b4e843f04cc2aaf6024
rodrigo <89788434+nachokm2@users.noreply.github.com>
2025-07-01 12:05:38 -0400

    Update sheets_query.py

--- sheets_query.py
+++ sheets_query.py
@@ -1,9 +1,19 @@
+import os
+import json
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 
 def consultar_matricula(identificador, tipo):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credenciales.json", scope)
+
+    # Leer el JSON desde variable de entorno
+    raw_creds = os.getenv("GOOGLE_CREDS_JSON")
+    if not raw_creds:
+        return {"error": "No se encontraron credenciales en la variable de entorno."}
+    
+    creds_dict = json.loads(raw_creds)
+    creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
+    
     client = gspread.authorize(creds)
     sheet = client.open("Copia de 2025 CONSOLIDADO MATRÍCULAS POSTGRADOS CHILE").worksheet("MATRÍCULAS CHILE")
     data = sheet.get_all_records()
